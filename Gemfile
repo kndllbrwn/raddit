5011dda7b71d3e1115452d8f65d14638b6ca6f4d
Kendall Brown <kndllbrwn1914@gmail.com>
2016-01-06 02:56:26 +0000

    Add structure and basic styling

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :development do
 end
 gem 'devise', '3.4.1'
 gem 'rails_12factor', group: :production
+gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 
 
 
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
@@ -11,6 +12,8 @@ class LinksController < ApplicationController
   # GET /links/1
   # GET /links/1.json
   def show
+  #show page bug fix  
+    @links = Link.all
   end
 
   # GET /links/new
@@ -66,6 +69,11 @@ class LinksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_link
       @link = Link.find(params[:id])
+    end
+
+    def authorized_user
+      @link = current_user.links.find_by(id: params[:id])
+      redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
